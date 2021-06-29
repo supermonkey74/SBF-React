260252204d147d9ea578d853f14f0ac804e5bac0
supermonkey74 <80603881+supermonkey74@users.noreply.github.com>
2021-06-29 13:54:30 -0500

    Same

---
+++ src/Components/Topbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Topbar.css';
+
+
+function Topbar() {
+  
+  return (
+    <>
+      <div className='topbar'>
+      
+      <a href="https://www.facebook.com/Spellbound-Farm-1577428402546846/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+      <a href="https://www.instagram.com/spellboundfarm/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
+      </div>
+    </>
+  );
+}
+
+export default Topbar;
--- src/Components/pages/Facility.js
+++ src/Components/pages/Facility.js
@@ -11,8 +11,29 @@ export default function Facility() {
         <div className="facility">
                 <h1>Our Facility</h1>
 
-                <p className="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati libero vitae accusamus, voluptates eum, quis magni pariatur ea vero error similique! Eaque eos, nihil corporis expedita minima aliquam autem culpa, commodi quos consequuntur, quaerat laborum dolores nulla fugit ullam quas itaque architecto totam eum alias harum quam vero placeat. Iste, nam sit laboriosam repellendus perspiciatis dolores dolore eveniet consequuntur porro est quod possimus amet! Molestiae adipisci excepturi similique dolorem quas vitae molestias laborum id dicta magnam aut, unde dolorum tenetur ducimus atque quasi, veritatis doloremque saepe fugiat voluptate repellendus consequuntur? Fuga ipsam adipisci, ab facilis porro a voluptates dicta modi.</p>
-                <p className="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati libero vitae accusamus, voluptates eum, quis magni pariatur ea vero error similique! Eaque eos, nihil corporis expedita minima aliquam autem culpa, commodi quos consequuntur, quaerat laborum dolores nulla fugit ullam quas itaque architecto totam eum alias harum quam vero placeat. Iste, nam sit laboriosam repellendus perspiciatis dolores dolore eveniet consequuntur porro est quod possimus amet! Molestiae adipisci excepturi similique dolorem quas vitae molestias laborum id dicta magnam aut, unde dolorum tenetur ducimus atque quasi, veritatis doloremque saepe fugiat voluptate repellendus consequuntur? Fuga ipsam adipisci, ab facilis porro a voluptates dicta modi.</p>
+                <p className="">Spellbound Farm is a Small Private Hunter/Jumper Training and Show Barn where the horse comes first!</p>
+                <p className="">We offer all inclusive training and boarding packages comprised of individualized custom care and training for the horse and rider. </p>
+                <p className="">Our brand new state of the art facility is located on 68 rolling acres with private gated access.</p>
+                <p className=""> Owners/trainers and caretakers live onsite to ensure hands on care for the horses.</p>
+<br />
+<h3>We offer the Following Amenities:</h3>
+        <div>
+                <ul>
+                    <li>Unlimited turn-out in pipe and no-climb paddocks (individual or small group)</li>
+                    <li>Spacious matted 14x12 airy stalls with large windows (fans and fly misters in the summer)</li>
+                    <li>Outdoor wash racks, indoor wash racks (with hot/cold water, pour in rubber flooring, heaters, and LED lighting)</li>
+                    <li>Four bay grooming areas</li>
+                    <li>Climate controlled client lounge and restroom</li>
+                    <li>Climate controlled client tack room with individual private lockers</li>
+                    <li>Large 135 X 250 covered and screened arena</li>
+                    <li>Custom Kiser footing</li>
+                    <li>LED lighting</li>
+                    <li>MacroAir Fans</li>
+                    <li>Tons of acreage room to hack and ride out</li>
+                    <li>Covered 6 horse free walker and additional 60 X 90 covered arena</li>
+
+                </ul>
+                </div>   
           </div>
                  <div className="arena-outer-container">
             
