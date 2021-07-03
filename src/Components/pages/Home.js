ca2c58cbc520af8d9fef11c72871123a10d7e908
supermonkey74 <80603881+supermonkey74@users.noreply.github.com>
2021-07-03 13:30:33 -0500

    Set animation Home Page

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -4,10 +4,12 @@ export default function Home() {
 
     return(
         <div className="home">
-        <div className="inside-home">
-        <h1>Welcome to Spellbound Farm</h1> 
-            <p>Hunters / Jumpers and Equitation</p>
+            <div className="inside-home">
+                <h1>Welcome to Spellbound Farm</h1> 
+                    <div className="inside-inside-home">
+                      <p>Hunters / Jumpers and Equitation</p>
+                    </div>       
             </div>
-            </div>
+        </div>
     )
 }
