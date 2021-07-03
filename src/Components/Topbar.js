6fa1a6be979243571031b9142e61bd7f9b29fb20
supermonkey74 <80603881+supermonkey74@users.noreply.github.com>
2021-07-03 15:07:35 -0500

    Added Email icon to topbar

--- src/Components/Topbar.js
+++ src/Components/Topbar.js
@@ -10,6 +10,7 @@ function Topbar() {
       
       <a href="https://www.facebook.com/Spellbound-Farm-1577428402546846/" target="_blank"><i class="fab fa-facebook-f"></i></a>
       <a href="https://www.instagram.com/spellboundfarm/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
+      <i class="fal fa-envelope-square"></i>
       </div>
     </>
   );
