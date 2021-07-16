da59473fcc0ab0dd60bebc28b7bd6315550f3fc8
supermonkey74 <80603881+supermonkey74@users.noreply.github.com>
2021-07-16 15:30:35 -0500

    update -

--- src/Components/pages/Training.js
+++ src/Components/pages/Training.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './training.css';
+import HotelCard from '../HotelCard'
 
 export default function Training() {
 
     return(
-        <>
+        <div className="training-container">
         <h1 className="training">Training</h1>
-        </>
+        <div className="training-container-inner">
+       
+                {/* {training.map( ({id, description, img, date},loupe) => (
+                    <div className="gallery" key={id}>
+                        <img src={loupe} alt="loupe"  className="loupe-img" width="100%" height="100%"/>
+                        <img src={img} alt="gallery-img" className="cover"/>
+                        <span className='title'>
+                            <h4>{description}, {date}</h4>
+                        </span>
+                    </div>
+                ))} */}
+
+                <HotelCard />
+                
+            </div>   
+
+
+        </div>
     )
 }
---
+++ src/Components/photostraining.js
@@ -0,0 +1,11 @@
+const training = [
+
+  { id: 1, src: '../Assets/arena-jumps.jpg', title: 'Lessons', description: 'lessons' },
+  { id: 2, src: '../Assets/lesson1728_n.png', title: 'Lessons', description: 'lessons' },
+  { id: 3, src: '../Assets/lesson1728_n.png', title: 'Lessons', description: 'lessons' },
+  { id: 4, src: '../Assets/lesson1728_n.png', title: 'Lessons', description: 'lessons' },
+  { id: 5, src: '../Assets/lesson1728_n.png', title: 'Lessons', description: 'lessons' },
+  
+];
+
+export default training;
